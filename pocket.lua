a3d00e9d3a63ef277da2104b7c73c49a61e2007d
Malte Schink <13069402+m4schini@users.noreply.github.com>
2021-03-25 10:48:28 +0100

    added import function

--- pocket.lua
+++ pocket.lua
@@ -1,7 +1,34 @@
-local uiLib = require("libs.ui")
-local osLib = require("libs.os")
-
 local MODEM_SIDE = "back"
+local PROTOCOL_RC = "RC"
+
+local keyBindings = {}
+keyBindings[keys.w] = "moveForward"
+keyBindings[keys.a] = "turnLeft"
+keyBindings[keys.s] = "moveBack"
+keyBindings[keys.d] = "turnRight"
+keyBindings[keys.space] = "moveUp"
+keyBindings[keys.leftCtrl] = "moveDown"
+keyBindings[keys.rightCtrl] = "moveDown"
+keyBindings[keys.r] = "digUp"
+keyBindings[keys.f] = "digForward"
+keyBindings[keys.v] = "digDown"
+keyBindings[keys.e] = "inventory"
+keyBindings[keys.t] = "scan"
+
+function Import(modname)
+    if not fs.exists("/libs/" .. modname) then
+        local request = http.get("https://raw.githubusercontent.com/m4schini/cc-programs/main/libs/" .. modname .. ".lua")
+        if request ~= nil then 
+            local handler = fs.open("/libs/" .. modname .. ".lua", "w");
+            handler.write(request.readAll())
+            handler.close()
+        end
+    end
+    return require("/libs/" .. modname)
+end
+
+local uiLib = Import("ui")
+local osLib = Import("os")
 
 function toHeading(number)
     local headingStr = "ERR"
@@ -26,25 +53,28 @@ local function UiPlaceholder(ui)
     ui.write("PLATZHALTER " .. os.clock(), colors.red)
 end
 
-local keyBindings = {}
-keyBindings[keys.w] = "moveForward"
-keyBindings[keys.a] = "turnLeft"
-keyBindings[keys.s] = "moveBack"
-keyBindings[keys.d] = "turnRight"
-keyBindings[keys.space] = "moveUp"
-keyBindings[keys.leftCtrl] = "moveDown"
-keyBindings[keys.rightCtrl] = "moveDown"
-keyBindings[keys.r] = "digUp"
-keyBindings[keys.f] = "digForward"
-keyBindings[keys.v] = "digDown"
-keyBindings[keys.e] = "inventory"
-keyBindings[keys.t] = "scan"
-
 local function UiRemoteControl(ui)
     rednet.open(MODEM_SIDE)
 
-    print("Enter ID of Turtle")
-    local id = tonumber(read())
+    local availableReceivers = rednet.lookup(PROTOCOL_RC)
+    local id = -1
+
+    local arType = type(availableReceivers)
+    if arType == "nil" then
+        rednet.close()
+        print("No receivers were found")
+        return;
+    elseif arType == "number" then
+        id = availableReceivers
+    else
+        for i, receiver in ipairs(availableReceivers) do
+            print(receiver)
+        end
+
+        print("Enter ID of Turtle")
+        id = tonumber(read())
+    end
+
 
     ui.clear()
     print("You're in control of the turtle")
@@ -63,62 +93,62 @@ local function UiRemoteControl(ui)
     while true do
         local event, key, is_held = os.pullEvent("key")
 
-        local action = keyBindings[key]
+        local action = keyBindings[key] or nil
         if action ~=  nil then
             rednet.send(id, {type="cmd", payload=action}, "RC")
-        end
 
+            local sender, message, protocol = rednet.receive()
         
-        local sender, message, protocol = rednet.receive()
-        
-        ui.clear()
-        ui.write("x=" .. message.position.x)
-        ui.write(" y=" .. message.position.y)
-        ui.write(" z=" .. message.position.z)
+            ui.clear()
+            ui.write("x=" .. message.position.x)
+            ui.write(" y=" .. message.position.y)
+            ui.write(" z=" .. message.position.z)
 
-        local heading = toHeading(message.position.heading or -1)
-        ui.write(" h=" .. heading)
+            local heading = toHeading(message.position.heading or -1)
+            ui.write(" h=" .. heading)
 
-        ui.printLine(2, colors.lightGray)
-        ui.setCursorPos(3)
+            ui.printLine(2, colors.lightGray)
+            ui.setCursorPos(3)
 
-        ui.print("above: ", colors.lightGray)
-        ui.print(message.scan.up.name or "air")
+            ui.print("above: ", colors.lightGray)
+            ui.print(message.scan.up.name or "air")
 
-        ui.print("infront: ", colors.lightGray)
-        ui.print(message.scan.front.name or "air")
+            ui.print("infront: ", colors.lightGray)
+            ui.print(message.scan.front.name or "air")
 
-        ui.print("below: ", colors.lightGray)
-        ui.print(message.scan.down.name or "air")
+            ui.print("below: ", colors.lightGray)
+            ui.print(message.scan.down.name or "air")
 
-        ui.printLine(9, colors.lightGray)
-        ui.setCursorPos(10)
+            ui.printLine(9, colors.lightGray)
+            ui.setCursorPos(10)
 
-        if message.payloadType == "INV" then
-            for i, slot in ipairs(message.payload) do
-                ui.write(slot.count)
+            if message.payloadType == "INV" then
+                for i, slot in ipairs(message.payload) do
+                    ui.write(slot.count)
 
-                local _, cY = term.getCursorPos()
-                term.setCursorPos(3, cY)
+                    local _, cY = term.getCursorPos()
+                    term.setCursorPos(3, cY)
 
-                ui.write(" | ", colors.gray)
-                ui.print(cutModId(slot.name))
+                    ui.write(" | ", colors.gray)
+                    ui.print(cutModId(slot.name))
+                end
+            elseif message.payloadType == "SCAN" then
+                for location, scan in pairs(message.payload) do
+                    ui.write(location, colors.lightGray)
+                    ui.write(": ", colors.lightGray)
+
+                    local _, cY = term.getCursorPos()
+                    term.setCursorPos(8, cY)
+                    ui.print(cutModId(scan.name or "minecraft:air"))
+                end
+            else
+                ui.write("response: ")
+                print(textutils.serialize(message.payload))
             end
-        elseif message.payloadType == "SCAN" then
-            for location, scan in pairs(message.payload) do
-                ui.write(location, colors.lightGray)
-                ui.write(": ", colors.lightGray)
-
-                local _, cY = term.getCursorPos()
-                term.setCursorPos(8, cY)
-                ui.print(cutModId(scan.name or "minecraft:air"))
-            end
-        else
-            ui.write("response: ")
-            print(textutils.serialize(message.payload))
         end
     end
 end
+
 
 
 local operatingSystem = osLib:new(nil, {
